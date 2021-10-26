e4dfe9c94d94f5843f5514ed3d67cca8d5a25806
n'semi makela <euloge.nsemi-makela@u-psud.fr>
2021-10-26 12:18:37 +0200

    maj

---
+++ src/frog/Frog.java
@@ -0,0 +1,20 @@
+package frog;
+
+import gameCommons.Game;
+import gameCommons.IFrog;
+import util.*;
+
+
+public class Frog implements IFrog {
+
+	private Game game;
+	private Case position;
+	private Direction direction;
+
+	public Frog(Game game) {
+		this.game = game;
+
+
+
+
+	}
